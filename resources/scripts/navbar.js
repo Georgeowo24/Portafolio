9efad966e51eb6bbbbd2fb9a865c857f72d08a85
Jorge Torres Romero <jtorresromero30@gmail.com>
2025-10-04 21:58:59 -0600

    Añadiendo responsividad

--- resources/scripts/navbar.js
+++ resources/scripts/navbar.js
@@ -1,5 +1,14 @@
 window.addEventListener('scroll', () => {
-    const nav = document.getElementById('navbar');
-    if (window.scrollY > 20) nav.classList.add('scrolled');
-    else nav.classList.remove('scrolled');
+  const nav = document.getElementById('navbar');
+  nav.classList.toggle('scrolled', window.scrollY > 20);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('mobile-menu-open');
+    menuToggle.classList.toggle('active');
+  });
 });
