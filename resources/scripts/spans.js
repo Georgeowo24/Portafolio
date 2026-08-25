82fb9ffd4140fefc4be86e71e3084f04cff16f30
Georgeowo24 <133627965+Georgeowo24@users.noreply.github.com>
2026-08-25 13:10:18 +0000

    Deploying to gh-pages from @ Georgeowo24/Portafolio@5764cc76b84e268e9156965583a5761ae20e19c4 🚀

---
+++ resources/scripts/spans.js
@@ -0,0 +1,8 @@
+window.initSpans = function() {
+    document.addEventListener("DOMContentLoaded", () => {   
+        const techItems = document.querySelectorAll(".tech-list span");
+        techItems.forEach((item, index) => {
+        item.style.animationDelay = `${index * 0.1 + 0.3}s`;
+        });
+    });
+};
